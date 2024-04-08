c66b2bbc80a434859254550c2edf9b520a1df3ec
Thomas Himmer <139979108+thomas-himmer@users.noreply.github.com>
2024-04-08 21:13:57 +0200

    Extend `timeseries_utils.py` with function `example_usage()`

--- timeseries_utils.py
+++ timeseries_utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def monthly_climatology(df):
     """Compute the monthly climatology (seasonal cycle) of a given time series."""
     return df.groupby(df.index.month).mean()
@@ -10,3 +14,17 @@ def anomaly(df):
         return (x - x.mean()) / x.std()
 
     return df.groupby(df.index.month).transform(standardize)
+
+
+def example_usage():
+    # Example usage:
+    temperature_data = pd.read_csv('temperature_data.csv', parse_dates=True, index_col=0)
+    temp_clim = monthly_climatology(temperature_data)
+    temp_anom = anomaly(temperature_data)
+    temp_clim.plot()
+    temp_anom.plot()
+    plt.show()
+
+
+if __name__ == '__main__':
+    example_usage()

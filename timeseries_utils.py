7266a8f7107732298f489650f7fb0cc44ccba361
mahmels <el-sayed.mahmoud@hotmail.com>
2024-04-22 20:54:55 +0200

    Make tests and ruff fail

--- timeseries_utils.py
+++ timeseries_utils.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def monthly_climatology(df):
     """Compute the monthly climatology (seasonal cycle) of a given time series."""
-    return df.groupby(df.index.month).mean()
+    return df.groupby(df.index.month).max()
 
 
 def anomaly(df):
     """Compute the anomaly of a given time series by removing the climatology."""
 
     def standardize(x):
-        return (x - x.mean()) / x.std()
+        return x - x.mean()
 
     return df.groupby(df.index.month).transform(standardize)
 
